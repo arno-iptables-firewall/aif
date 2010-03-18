2ecb3a2de715abbcd4f08f5c04740eb9a47cf47d
arnova <arnova@2cdb06df-4b6c-47a8-a790-b77aef9f425a>
2010-03-18 08:26:28 +0000

    added: Detection of (old) plugins with different priority than the current
    git-svn-id: https://rocky.eld.leidenuniv.nl/svn/aif/trunk@232 2cdb06df-4b6c-47a8-a790-b77aef9f425a

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MY_VERSION="1.02c"
+MY_VERSION="1.03"
 
 # ------------------------------------------------------------------------------------------
 #                           -= Arno's iptables firewall =-
@@ -229,10 +229,10 @@ get_user_yn()
 }
 
 
-detect_18_version()
+check_18_version()
 {
   if [ -e "/etc/init.d/arno-iptables-firewall" ] && grep -q "^MY_VERSION=" "/etc/init.d/arno-iptables-firewall"; then
-    if get_user_yn "WARNING: An old version is still installed. Removing it first, is *STRONGLY* recommended. Remove (Y/N)?" "y"; then
+    if get_user_yn "WARNING: An old version is still installed. Removing it first is *STRONGLY* recommended. Remove (Y/N)?" "y"; then
       rm -fv /etc/init.d/arno-iptables-firewall
       mv -fv /etc/arno-iptables-firewall/custom-rules /etc/arno-iptables-firewall/custom-rules.old
       mv -fv /etc/arno-iptables-firewall/firewall.conf /etc/arno-iptables-firewall/firewall.conf.old
@@ -241,6 +241,22 @@ detect_18_version()
     fi
   fi
 }
+
+
+# Check plugins for (old) versions with different priority
+check_plugins()
+{
+  unset IFS
+  for plugin in ./share/arno-iptables-firewall/plugins/*; do
+    plugin_name="$(basename "$plugin" |sed 's/^[0-9]*//')"
+      
+    find /usr/local/arno-iptables-firewall/plugins/ -maxdepth 1 -name "*.plugin" |grep "^[0-9]*$plugin_name$" |while read FN; do
+    if [ "$(basename "$fn")" != "$(basename "$plugin")" ]; then
+      rm -fv "$plugin"
+    fi
+  done
+}
+
 
 # main line:
 AIF_VERSION="$(grep "MY_VERSION=" ./bin/arno-iptables-firewall |sed -e "s/^MY_VERSION=\"//" -e "s/\"$//")"
@@ -260,7 +276,7 @@ if ! get_user_yn "Continue install (Y/N)?" "n"; then
 fi
 
 # Make sure there still isn't an old version installed
-detect_18_version;
+check_18_version;
 
 copy_overwrite ./bin/ /usr/local/sbin/
 
@@ -273,14 +289,16 @@ mkdir -pv /usr/local/share/man/man8
 gzip -c -v ./share/man/man8/arno-iptables-firewall.8 >/usr/local/share/man/man8/arno-iptables-firewall.8.gz
 gzip -c -v ./share/man/man1/arno-fwfilter.1 >/usr/local/share/man/man8/arno-fwfilter.1.gz
 
-mkdir -pv /etc/arno-iptables-firewall/plugins
-copy_ask_if_exist ./etc/arno-iptables-firewall/plugins/ /etc/arno-iptables-firewall/plugins/
-
 copy_ask_if_exist ./etc/init.d/arno-iptables-firewall /etc/init.d/
 
 copy_overwrite ./etc/arno-iptables-firewall/firewall.conf /etc/arno-iptables-firewall/firewall.conf.dist
 copy_skip_if_exist ./etc/arno-iptables-firewall/custom-rules /etc/arno-iptables-firewall/
 copy_ask_if_exist ./etc/arno-iptables-firewall/firewall.conf /etc/arno-iptables-firewall/
+
+mkdir -pv /etc/arno-iptables-firewall/plugins
+copy_ask_if_exist ./etc/arno-iptables-firewall/plugins/ /etc/arno-iptables-firewall/plugins/
+
+check_plugins;
 
 echo ""
 echo "** Install done **"
